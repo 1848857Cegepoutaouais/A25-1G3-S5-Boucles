0ab25f5efdc274c3b7d0f1a7ad4fdc74071d884e
1848857Cegepoutaouais <1848857@cegepoutaouais.qc.ca>
2025-09-19 13:33:14 -0400

    Exercice completé avec If, for, Isinstance, listes, input

--- ExDebug1.py
+++ ExDebug1.py
@@ -3,47 +3,71 @@ def environnement_optimal(temp, poussiere, humidite):
     Vérifie si l'environnement d'un ordinateur est optimal.
 
     Paramètres :
-    - temp : température en °C (int ou float)
+    - temp : température
     - poussiere : niveau de poussière ("faible", "moyen", "élevé")
-    - humidite : taux d’humidité en % (int ou float)
+    - humidite : humidité
 
     Retour :
-    - "Tout est sous contrôle!" si toutes les conditions sont respectées
-    - "Environnement non optimal" sinon (après avoir affiché les problèmes détectés)
+    - "Tout est sous contrôle!" si tout est bon
+    - "Environnement non optimal" les problèmes sinon
     """
-
     alerte = False
+    message = ""
 
-    # Vérification température
-    if temp < 18:
-        print("Température trop basse")
-        alerte = True
-    elif temp > 27:
-        print("Température trop élevée")
-        alerte = True
-
-    # Vérification humidité
-    if humidite <= 30:
-        print("Humidité trop basse")
-        alerte = True
-    elif humidite >= 50:
-        print("Humidité trop élevée")
+    if isinstance(temp, int) or isinstance(temp, float):
+        if temp < 18:
+            message += "Température trop basse \n"
+            alerte = True
+        elif temp > 27:
+            message += "Température trop élevée \n"
+            alerte = True
+    else:
+        message += "La température est exprimé en chiffre!"
         alerte = True
 
-    # Vérification poussière
-    if poussiere != "faible":
-        print("Trop de poussière")
+    if isinstance(poussiere, str):
+        if poussiere != "faible" and poussiere != "Faible":
+            message += "Trop de poussière\n"
+            alerte = True
+    else:
+        message += "La poussière est exprimé en string, par faible, moyen et élevé!\n"
         alerte = True
 
-    # Retour final
+    if isinstance(humidite, int) or isinstance(humidite, float):
+        if humidite <= 30 :
+            message += "Il fait pas assez humide\n"
+            alerte = True
+        elif humidite >= 50:
+            message += "Il fait trop humide\n"
+            alerte = True
+    else:
+        message += "L'humidité est exprimé en chiffre!"
+        alerte = True
+
+    if message != "":
+        print(message)
+
     if not alerte:
         return "Tout est sous contrôle!"
     else:
         return "Environnement non optimal"
 
-
 if __name__ == "__main__":
-    temp = float(input("Entrez la température: "))
-    poussiere = input("Entrez le niveau de poussière: ")
-    humidite = float(input("Entrez l'humidité: "))
-    print(environnement_optimal(temp, poussiere, humidite))
+    i = 0
+    ls_temp = [20, 21, 40, 100, 0]
+    ls_dust = ["faible", "faible", "faible", "élevé", "moyen"]
+    ls_wet = [40, 30, 45, 99, 0]
+    nb_ordi = int(input("Combien d'ordinateur à tester?"))
+
+    for i in range(nb_ordi):
+        temp = float(input("Entrer la température"))
+        dust = input("entrer le niveau de poussière")
+        wet = float(input("Entrer le taux d'humidité"))
+        ls_temp.append(temp)
+        ls_dust.append(dust)
+        ls_wet.append(wet)
+
+
+    for i in range(len(ls_temp)):
+        print(environnement_optimal(ls_temp[i], ls_dust[i], ls_wet[i]))
+
