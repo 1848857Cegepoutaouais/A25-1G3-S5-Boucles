f96b332af011fe6e57d88df7aea1e51117c29ac1
1848857Cegepoutaouais <184857@cegepoutaouais.qc.ca>
2025-09-22 08:51:01 -0400

    Test Unitaire fait

--- test_unitaire_exercice1.py
+++ test_unitaire_exercice1.py
@@ -29,4 +29,30 @@ def test_environnement_optimal_2():
     # Assert : vérifier si le résultat obtenu correspond au résultat attendu
     assert resultat_attendu == resultat_obtenu
 
+#Test Unitaire pour la fonction environnement_optimal
+def test_environnement_optimal_3():
+    #Arange : préparer les valeurs des variables d'entrées et le résultat attendu
+    temp = 25
+    poussiere = "faible"
+    humidite = 20
+    resultat_attendu = "Environnement non optimal"
 
+    # Act : appeler la fonction
+    resultat_obtenu = environnement_optimal(temp, poussiere, humidite)
+
+    # Assert : vérifier si le résultat obtenu correspond au résultat attendu
+    assert resultat_attendu == resultat_obtenu
+
+#Test Unitaire pour la fonction environnement_optimal
+def test_environnement_optimal_4():
+    #Arange : préparer les valeurs des variables d'entrées et le résultat attendu
+    temp = 30
+    poussiere = "moyen"
+    humidite = 25
+    resultat_attendu = "Environnement non optimal"
+
+    # Act : appeler la fonction
+    resultat_obtenu = environnement_optimal(temp, poussiere, humidite)
+
+    # Assert : vérifier si le résultat obtenu correspond au résultat attendu
+    assert resultat_attendu == resultat_obtenu
